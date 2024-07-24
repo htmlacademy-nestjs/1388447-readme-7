9827b28e87f2741c938c2aa51735668fdc278791
Alexandr10m <serbackij@gmail.com>
2024-07-24 14:18:32 +0300

    adds libs

---
+++ project/libs/shared/core/src/index.ts
@@ -0,0 +1,5 @@
+export {Entity} from './lib/classes/entity';
+
+export {User} from './lib/types/user.interface';
+export {AuthUser} from './lib/types/auth-user.interface';
+export {UserRole} from './lib/types/user-role.enum';
